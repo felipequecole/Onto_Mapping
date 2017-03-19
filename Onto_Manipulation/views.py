d23d873a2f3a22767d4ab6427935cb0d41afce09
pedro <pedro.migliatti@gmail.com>
2017-03-19 01:02:35 -0300

    Pegando as informações do form de edição de categoria, mas não consegui fazer um append no xml ainda

--- Onto_Manipulation/views.py
+++ Onto_Manipulation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from Onto_Manipulation import Onto_mapping
 import xml.etree.ElementTree as etree
+import os
 
 def relation(request):
     dict = Onto_mapping.return_dict()
@@ -14,6 +15,41 @@ def edit(request):
     return render(request, 'Onto_Manipulation/edit.html', {})
 
 def edit_cat(request):
+    if request.method == 'POST':
+        categoryName = request.POST['name']
+        englishName = request.POST['english']
+        humanFormat = request.POST['human']
+        # visibleTrue = request.POST['options1']
+        # visibleFalse = request.POST['options2']
+        generalization = request.POST['generalization']
+        mutexException = request.POST['mutex']
+        knowNegatives = request.POST['negatives']
+        seedInstaves = request.POST['instances']
+        seedPatterns = request.POST['patterns']
+        conceptSynonyms = request.POST['synonyms']
+        author = request.POST['author']
+        description = request.POST['desc']
+        comment = request.POST['comment']
+
+        pwd = os.path.dirname(__file__)
+        xml_path = os.path.join(pwd, 'static/Onto_Manipulation/data/ontology.xml')
+        xml = etree.parse(xml_path)
+        root = etree.Element('Ontology')
+        cats = etree.SubElement(root, 'Categories')
+        cat = etree.SubElement(cats, 'Category')
+        cat.set('id',"999")
+
+
+        test = etree.SubElement(cat, 'categoryName')
+        test.text = (str(categoryName)).encode('utf8')
+        test = etree.SubElement(cat, 'englishName')
+        test.text = (str(englishName)).encode('utf8')
+        test = etree.SubElement(cat, 'humanFormat')
+        test.text = (str(humanFormat)).encode('utf8')
+
+        xml.write('static/Onto_Manipulation/data/ontology.xml')
+
+
     return render(request, 'Onto_Manipulation/edit_cat.html', {})
 
 def edit_rel(request):
