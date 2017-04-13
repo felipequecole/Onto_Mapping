5a176ee61861111c828ccb4ccd27355268eee227
pedro <pedro.migliatti@gmail.com>
2017-04-13 02:03:24 -0300

    Adicionado o texto sobre o grupo e diminuida o div laranja

--- Onto_Manipulation/static/Onto_Manipulation/dialog.js
+++ Onto_Manipulation/static/Onto_Manipulation/dialog.js
@@ -1,9 +1,5 @@
 $(function () {
     var ol = $('#upload ol');
-    //Quando clica em a ele clica em input
-    $('#drop a').click( function () {
-        $(this).parent().find('input').click();
-    });
     //Abre o dialog
     $('body').on('click', '#openUpload', function (e) {
         e.preventDefault();
